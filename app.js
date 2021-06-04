c90566698ddbbd1ca6fa062ec1b0894517795716
denefc <mehmet_7kezman@hotmail.com>
2021-06-04 22:56:23 +0300

    done from beginnig to postman

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const mongoose=require('mongoose')
+const pageRoute=require('./routes/pageRoute')
+const courseRoute=require('./routes/courseRoute')
+
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Index sayfası");
+//connect db
+mongoose.connect('mongodb://localhost/smartedu-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(()=> {
+  console.log('DB Connected Successfully')
 });
+
+//Template engine
+app.set("view engine","ejs")
+
+//Middlewares
+app.use(express.static("public"))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+//routes
+app.use("/", pageRoute)
+app.use("/courses",courseRoute)
+
+
 
 const port = 3000;
 app.listen(port, () => {
